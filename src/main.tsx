5ec471bbc98f781f0a0c3fe65a1d81c9dcd5891a
hisreal123 <omosgoerge@gmail.com>
2023-02-23 13:32:44 +0100

    ✅ ⚡️  run yarn build in terminal

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,6 @@ import './styles/index.scss'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  ,
 )
